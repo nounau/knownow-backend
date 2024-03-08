20549c2204c372b18250d077e28732b280bf9dfd
Rohan Sharma <rohansharma996034@gmail.com>
2024-03-09 00:11:07 +0530

    trying changes

--- app.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from dotenv import load_dotenv
-
-app = create_app()
-load_dotenv('.flaskenv') #the path to your .env file (or any other file of environment variables you want to load)
-
-if __name__ == "__main__":
-    app.run()
